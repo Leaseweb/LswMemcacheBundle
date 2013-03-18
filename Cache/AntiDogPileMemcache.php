228c83a90797407e6e0a99bcd5c602cee8385d89
Maurits van der Schee <m.vanderschee@leaseweb.com>
2013-03-18 22:23:32 +0100

    Properly serialize objects for ADP

--- Cache/AntiDogPileMemcache.php
+++ Cache/AntiDogPileMemcache.php
@@ -25,6 +25,17 @@ class AntiDogPileMemcache
         $this->memcache = new LoggingMemcache($debug, $persistentId);
     }
 
+    /**
+     * Function to get value by key using Anti-Dog-Pile algorithm.
+     * NB: On every invalidation only one call will return false,
+     * other calls will just return the previous value. Whenever false
+     * is returned it is the programmers responsibility to call setAdp()
+     * to set a new value.
+     *
+     * @param string $key Key of the value you are trying to retrieve
+     *
+     * @return string Value read from cache or false if cache is stale
+     */
     public function getAdp($key)
     {
         $value = $this->memcache->get($key, null, &$cas);
@@ -32,10 +43,11 @@ class AntiDogPileMemcache
             return false;
         }
         list($exp, $ttl, $val) = explode('|', $value, 3);
+        $val = json_decode($val);
 
         $time = time();
         if ($time>$exp) {
-            $value = implode('|', array($time+$ttl, $ttl, $val));
+            $value = implode('|', array($time+$ttl, $ttl, json_encode($val)));
             $result = $this->memcache->cas($cas, $key, $value, 0);
 
             if ($result) {
@@ -46,13 +58,22 @@ class AntiDogPileMemcache
         return $val;
     }
 
+    /**
+     * Function to set value by key using Anti-Dog-Pile algorithm.
+     *
+     * @param string $key   Key of the value you are storing
+     * @param string $value Value you want to store
+     * @param int    $ttl   Seconds before the cache item is returning false (once)
+     *
+     * @return boolean True on success, false on failure
+     */
     public function setAdp($key, $value, $ttl=0)
     {
         if ($ttl === 0) {
             $ttl = self::MAX_TTL;
         }
         $time = time();
-        $value = implode('|', array($time+$ttl, $ttl, $value));
+        $value = implode('|', array($time+$ttl, $ttl, json_encode($value)));
         $result = $this->memcache->set($key, $value, 0);
 
         return $result;
